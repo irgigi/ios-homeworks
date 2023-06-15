3188ebbbc2982daaa2f83180cbc7b60ebe45f185
Мамуля <mamula@MacBook-Air-Evgenia.local>
2023-06-15 16:00:44 +0500

    изменение кнопки инфо

--- Profile/ProfileHeaderView.swift
+++ Profile/ProfileHeaderView.swift
@@ -2,16 +2,33 @@
 //  ProfileHeaderView.swift
 //  Navigation
 //
-//  Created by Мамуля on 10.06.2023.
 //
 
 import UIKit
 
 class ProfileHeaderView: UIView {
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
+   // override func awakeFromNib() {
+   //     super.awakeFromNib()
+    
+    
+    override func draw(_ rect: CGRect) {
         
+        let height = rect.height
+        let width = rect.width
+        let color: UIColor = .systemYellow
+        
+        let photo = CGRect(
+            x: 16.0,
+            y: 16.0,
+            width: 120.0,
+            height: 120.0
+            )
+        
+     //   let bpath: UIBezierPath = UIBezierPath(rect: photo)
+        
+        color.set()
+      //  bpath.stroke()
     }
 }
 
--- Profile/ProfileViewController.swift
+++ Profile/ProfileViewController.swift
@@ -2,18 +2,28 @@
 //  ProfileViewController.swift
 //  Navigation
 //
-//  Created by Мамуля on 10.06.2023.
 //
 
 import UIKit
 
 class ProfileViewController: UIViewController {
     
+    let profileHeaderView = ProfileHeaderView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .lightGray
+        view.addSubview(profileHeaderView)
+        
+        
     }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        profileHeaderView.frame = view.frame
+    }
+    
     
 }
 

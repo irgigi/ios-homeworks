5890a28e55943c4a580178bf011ed78afddfd4f9
Мамуля <mamula@MacBook-Air-Evgenia.local>
2023-06-17 08:24:15 +0500

    label?

--- Profile/ProfileHeaderView.swift
+++ Profile/ProfileHeaderView.swift
@@ -10,26 +10,61 @@ class ProfileHeaderView: UIView {
     
    // override func awakeFromNib() {
    //     super.awakeFromNib()
-    
-    
-    override func draw(_ rect: CGRect) {
-        
-        let height = rect.height
-        let width = rect.width
-        let color: UIColor = .systemYellow
-        
-        let photo = CGRect(
-            x: 16.0,
-            y: 16.0,
-            width: 120.0,
-            height: 120.0
-            )
-        
-     //   let bpath: UIBezierPath = UIBezierPath(rect: photo)
-        
-        color.set()
-      //  bpath.stroke()
+ 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        self.addSubview(nameLabel)
     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        //создание UIImageView с изображением
+        let imageView = UIImageView(
+            frame: CGRect(x: 16, y: 16, width: 120, height: 120)
+            )
+        imageView.image = UIImage(named: "Felix")
+        imageView.contentMode = .scaleToFill
+        imageView.clipsToBounds = true
+        
+        //создание круглой рамки
+        imageView.layer.cornerRadius = imageView.frame.size.width / 2
+        imageView.layer.borderWidth = 3.0
+        imageView.layer.borderColor = UIColor.white.cgColor
+    /*
+        //создание label
+        let label = UILabel(
+            frame: CGRect(x: imageView.frame.midX, y: 27, width: 100, height: 50 )
+            )
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.textColor = .black
+        label.text = "Cat Felix"
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+     */
+        //добавление UIImageView на UIView
+        addSubview(imageView)
+       
+        
+    }
+
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.textColor = .black
+        label.text = "Cat Felix"
+        //label.textAlignment = .center
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+        
+    }()
+  
 }
 
 
--- Profile/ProfileViewController.swift
+++ Profile/ProfileViewController.swift
@@ -8,14 +8,27 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    let profileLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.textColor = .black
+        label.text = "Profile"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+       }()
+    
     let profileHeaderView = ProfileHeaderView()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .lightGray
+        title = profileLabel.text
         view.addSubview(profileHeaderView)
         
+        setupConstraints()
         
     }
     
@@ -23,6 +36,35 @@ class ProfileViewController: UIViewController {
         super.viewWillLayoutSubviews()
         profileHeaderView.frame = view.frame
     }
+    
+    func setupConstraints() {
+        let safeAreaGuide = view.safeAreaLayoutGuide
+        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+          /*  profileHeaderView.leadingAnchor.constraint(
+                equalTo: safeAreaGuide.leadingAnchor,
+                constant: 20
+            ),
+            profileHeaderView.trailingAnchor.constraint(
+                equalTo: safeAreaGuide.trailingAnchor,
+                constant: -20
+            ),
+        */
+            profileHeaderView.topAnchor.constraint(
+                equalTo: safeAreaGuide.topAnchor,
+                constant: 10
+            ),
+            
+            profileHeaderView.bottomAnchor.constraint(
+                equalTo: safeAreaGuide.bottomAnchor,
+                constant: 10
+            )
+            
+            //profileHeaderView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.8)
+        ])
+    }
+    
+    
     
     
 }

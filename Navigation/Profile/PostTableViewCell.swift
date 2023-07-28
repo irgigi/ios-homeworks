2804333154d3b60671b6e42ec1ac821e53e59305
Мамуля <mamula@MacBook-Air-Evgenia.local>
2023-07-28 20:54:10 +0500

    full

--- Navigation/Profile/PostTableViewCell.swift
+++ Navigation/Profile/PostTableViewCell.swift
@@ -127,11 +127,21 @@ class PostTableViewCell: UITableViewCell {
         }
         
     }
+    
+  
 
     func update(_ model: PostModel) {
         
+        let img_processor = ImageProcessor()
+        let img = UIImage(named: model.image) ?? UIImage()
+        img_processor.processImage(sourceImage: img, filter: .sepia(intensity: 0.4)) {
+            processedImage in
+         
+                imagePost.image = processedImage
+            
+        }
         autorLabel.text = model.author
-        imagePost.image = UIImage(named: model.image)
+        //imagePost.image = UIImage(named: model.image)
         descriptionLabel.text = model.description
         likesLabel.text! += String(describing: model.likes)
         viewsLabel.text! += String(describing: model.views)

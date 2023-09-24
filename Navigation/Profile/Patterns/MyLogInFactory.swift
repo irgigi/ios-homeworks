964c1a8dafbbd48d775269eb8a084d6bef521bcd
irgigi <79409202+irgigi@users.noreply.github.com>
2023-09-24 14:58:49 +0500

    better

--- Navigation/Profile/Patterns/MyLogInFactory.swift
+++ Navigation/Profile/Patterns/MyLogInFactory.swift
@@ -8,7 +8,6 @@ import UIKit
 struct MyLogInFactory: LoginFactory {
     
     func makeLoginInspector() -> LoginInspector {
-        let loginInspector = LoginInspector()
         return LoginInspector()
     }
     
--- Navigation/Profile/Profile.swift
+++ Navigation/Profile/Profile.swift
@@ -13,29 +13,7 @@ struct Profile {
 
 extension Profile {
     static func make() -> [Profile] {
-        [
-        (Profile(img: "felix1")),
-        (Profile(img: "felix2")),
-        (Profile(img: "felix3")),
-        (Profile(img: "felix4")),
-        (Profile(img: "felix5")),
-        (Profile(img: "felix6")),
-        (Profile(img: "felix7")),
-        (Profile(img: "felix8")),
-        (Profile(img: "felix9")),
-        (Profile(img: "felix10")),
-        (Profile(img: "felix11")),
-        (Profile(img: "felix12")),
-        (Profile(img: "felix13")),
-        (Profile(img: "felix14")),
-        (Profile(img: "felix15")),
-        (Profile(img: "felix16")),
-        (Profile(img: "felix17")),
-        (Profile(img: "felix18")),
-        (Profile(img: "felix19")),
-        (Profile(img: "felix20")),
-        (Profile(img: "felix21"))
-        ]
+        (1...21).compactMap{ Profile(img: "felix\($0)") }
     }
     
 }

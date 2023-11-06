728cec1bd0300ff201b77369cbd1447ad29b58b0
irgigi <79409202+irgigi@users.noreply.github.com>
2023-11-06 13:39:38 +0500

    init

---
+++ Navigation/Profile/Post.swift
@@ -0,0 +1,46 @@
+//
+//  Post.swift
+//  Navigation
+//
+
+//
+
+import Foundation
+
+struct PostModel {
+    let author: String
+    let description: String
+    let image: String
+    let likes: Int
+    let views: Int
+}
+
+extension PostModel {
+    static func make() -> [PostModel] {
+        [
+            PostModel(author: "Felix",
+                      description: "Me in 2 mounth",
+                      image: "felix1",
+                      likes: 1,
+                      views: 1),
+            
+            PostModel(author: "Felix",
+                      description: "Me in 12 years old",
+                      image: "felix2",
+                      likes: 1,
+                      views: 1),
+            
+            PostModel(author: "Felix",
+                      description: "Me and my favorite food",
+                      image: "felix3",
+                      likes: 1,
+                      views: 1),
+            
+            PostModel(author: "Felix",
+                      description: "My rest on the computer desk",
+                      image: "felix4",
+                      likes: 1,
+                      views: 1),
+        ]
+    }
+}
